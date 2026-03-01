324c0e417b968cc1e230e60a0f4750db0755cc6e
shackleb0lt <kawadeaniruddha@gmail.com>
2026-03-01 19:42:49 +0530

    Updated run and cmake script

--- run.sh
+++ run.sh
@@ -9,17 +9,31 @@ set -e
 # 1. Create directory if it doesn't exist
 mkdir -p build
 
-# 2. Only run CMake if the cache doesn't exist
-if [ ! -f "build/CMakeCache.txt" ]; then
+VCPKG_ROOT="${VCPKG_ROOT:$HOME/vcpkg}"
+VCPKG_TOOLCHAIN="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
 
-    cmake -S . -B build -G "Ninja" \
-        -DCMAKE_BUILD_TYPE=Debug \
-        -DCMAKE_TOOLCHAIN_FILE=/home/qubit/vcpkg/scripts/buildsystems/vcpkg.cmake 
-else
-    echo "--- CMake cache found, skipping configuration ---"
+if [[-f "$VCPKG_TOOLCHAIN"]]; then
+    echo "Error: vcpkg toolchain not found at $VCPKG_TOOLCHAIN"
+    exit 1
 fi
 
-ninja -C build
+if command -v ninja &> /dev/null; then
+    CMAKE_GENERATOR="Ninja"
+    BUILD_CMD="ninja -C build"
+elif command -v make &> /dev/null; then
+    CMAKE_GENERATOR="Unix Makefiles"
+    BUILD_CMD="make -C build"
+else
+    echo "Neither ninja nor make found."
+    exit 1
+fi
+
+
+cmake -S . -B build -G $CMAKE_GENERATOR \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_TOOLCHAIN_FILE="$VCPKG_TOOLCHAIN"
+
+$BUILD_CMD
 
 # 4. Set permissions
 # echo "--- Applying capabilities ---"
